0425e0103e784e355f4cc01b16b370b28b609666
Emanuel <kraemerseele@gmx.de>
2013-06-09 19:44:44 +0200

    erste angularisierung

---
+++ static/assets/scripts/controller/signup.js
@@ -0,0 +1,16 @@
+(function (angular) {
+
+	'use strict';
+
+	angular
+		.module('lunchroulette')
+		.controller('SignUpCtrl', ['$scope', 'SIGNUP_PATH', function ($scope, SIGNUP_PATH) {
+
+			$scope.submitForm = function submitForm() {
+				console.log($scope);
+				$scope.formError = !$scope.LogInForm.$valid;
+			};
+
+		}]);
+
+})(angular);
